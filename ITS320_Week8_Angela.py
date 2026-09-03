4da6347976f89ecb31fe7401e8dd677a4902b7eb
Angi-Adema <dr.adema@gmail.com>
2026-09-03 15:37:08 -0600

    moved the users list of student and admin objects to below the User, Student, and Admin classes.

--- ITS320_Week8_Angela.py
+++ ITS320_Week8_Angela.py
@@ -1,17 +1,13 @@
 # Create a Student Course Registration system where students and admin can log in to 
 # complete tasks such as registering for a course or for the admin, print a report or add a new course.
 
-# Create a list of established user objects.
-users = [
-    {"username": "admin", "password": "password", "role": "admin"},
-    {"username": "student1", "password": "password1", "role": "student"},
-    {"username": "student2", "password": "password2", "role": "student"}
-]
-
+# Create a class to represent a user with attributes for username, password, and role (student or admin).
 class User:
-    # username
-    # password
-    pass
+    # constructor to initialize the user object with username, password, and role
+    def __init__(self, username, password, role):
+        self.username = username
+        self.password = password
+        self.role = role
 
 class Student(User):
     # student-specific info
@@ -21,3 +17,9 @@ class Admin(User):
     # admin-specific info
     pass
 
+# Create a list of established user objects.
+users = [
+    Admin("admin", "password", "admin"),
+    Student("student1", "password1", "student"),
+    Student("student2", "password2", "student")
+]
